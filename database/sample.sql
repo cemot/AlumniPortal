82bbc8bd7b577a4b97189f06f1f41ec2282f56f4
lucifer-harish <harish.sreenivasan664@gmail.com>
2017-06-30 11:07:22 +0530

    Add files via upload

---
+++ database/sample.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[OldRegister] (
+    [Name]          VARCHAR (25) NOT NULL,
+    [Address]       VARCHAR (50) NOT NULL,
+    [Register No]   NCHAR (15)   NOT NULL,
+    [Joining Date]  DATE NOT NULL,
+    [UserName]      VARCHAR (25) NOT NULL,
+    [Password]      VARCHAR (25) NOT NULL,
+    [Email ID]      VARCHAR (50) NOT NULL,
+    [DOB]           VARCHAR (25) NOT NULL,
+    [Contact No]    NCHAR (15)   NOT NULL,
+    [Gender]        VARCHAR (6)  NOT NULL,
+    [Qualification] VARCHAR (20) NOT NULL,
+    [Works At]      VARCHAR (30) NULL,
+    [Designation]   VARCHAR (40) NULL,
+    [Register Date] VARCHAR (20) NOT NULL,
+    [Picture]       VARCHAR (50) NULL,
+    [status]        VARCHAR (5)  NOT NULL
+);
+
